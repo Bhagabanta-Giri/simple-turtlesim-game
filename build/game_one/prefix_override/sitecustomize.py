967ed771c7aa9827ef0aa2949215536efa90a3a6
Bhagabanta-Giri <bhagabantagiri@gmail.com>
2026-06-07 16:57:15 +0530

    Added gitignore and removed generated build/install/log files

--- build/game_one/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/lenovoi/Projects/My-Projects/simple-turtlesim-game/install/game_one'
